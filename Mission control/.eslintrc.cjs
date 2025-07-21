ed19219cc8f1481bfb9ad194df703af66608070d
srynyvas <31266370+srynyvas@users.noreply.github.com>
2025-07-22 00:22:54 +0530

    Add ESLint configuration

---
+++ Mission control/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'eqeqeq': 'error',
+    'curly': 'error',
+  },
+};
